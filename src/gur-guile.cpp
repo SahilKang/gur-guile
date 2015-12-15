2e1cda8154022cf112561c364d29bbb5aa3f8601
Sahil Kang <sahil.kang@asilaycomputing.com>
2015-12-15 02:25:27 -0800

    Add bool functions to determine char type
    - is-letter
    - is-accent
    - is-punc
    - is-digit
    - is-symbol

--- src/gur-guile.cpp
+++ src/gur-guile.cpp
@@ -20,6 +20,25 @@
 #include <libguile.h>
 #include <gur.hpp>
 
+template<typename T>
+static SCM is_type(const SCM &args, const T &func)
+{
+	const char* const str = scm_to_utf8_stringn(args, NULL);
+	bool is_type = func(str);
+
+	free(const_cast<char*>(str));
+
+	if (is_type)
+	{
+		return SCM_BOOL_T;
+	}
+
+	else
+	{
+		return SCM_BOOL_F;
+	}
+}
+
 static SCM gg_letters(SCM args)
 {
 	const char* const str = scm_to_utf8_stringn(args, NULL);
@@ -92,6 +111,31 @@ static SCM gg_unclobber(SCM args)
 	return g_str;
 }
 
+static SCM gg_is_letter(SCM args)
+{
+	return is_type(args, (bool (*)(const char* const&))&gur::is_letter);
+}
+
+static SCM gg_is_accent(SCM args)
+{
+	return is_type(args, (bool (*)(const char* const&))&gur::is_accent);
+}
+
+static SCM gg_is_punc(SCM args)
+{
+	return is_type(args, (bool (*)(const char* const&))&gur::is_punc);
+}
+
+static SCM gg_is_digit(SCM args)
+{
+	return is_type(args, (bool (*)(const char* const&))&gur::is_digit);
+}
+
+static SCM gg_is_symbol(SCM args)
+{
+	return is_type(args, (bool (*)(const char* const&))&gur::is_symbol);
+}
+
 static void init_gur_guile(void *unused)
 {
 	scm_c_define_gsubr("letters", 1, 0, 0, (scm_t_subr)&gg_letters);
@@ -102,6 +146,11 @@ static void init_gur_guile(void *unused)
 	scm_c_define_gsubr("comp", 1, 0, 0, (scm_t_subr)&gg_comp);
 	scm_c_define_gsubr("clobber", 1, 0, 0, (scm_t_subr)&gg_clobber);
 	scm_c_define_gsubr("unclobber", 1, 0, 0, (scm_t_subr)&gg_unclobber);
+	scm_c_define_gsubr("is-letter", 1, 0, 0, (scm_t_subr)&gg_is_letter);
+	scm_c_define_gsubr("is-accent", 1, 0, 0, (scm_t_subr)&gg_is_accent);
+	scm_c_define_gsubr("is-punc", 1, 0, 0, (scm_t_subr)&gg_is_punc);
+	scm_c_define_gsubr("is-digit", 1, 0, 0, (scm_t_subr)&gg_is_digit);
+	scm_c_define_gsubr("is-symbol", 1, 0, 0, (scm_t_subr)&gg_is_symbol);
 
 	scm_c_export("letters", NULL);
 	scm_c_export("accents", NULL);
@@ -111,6 +160,11 @@ static void init_gur_guile(void *unused)
 	scm_c_export("comp", NULL);
 	scm_c_export("clobber", NULL);
 	scm_c_export("unclobber", NULL);
+	scm_c_export("is-letter", NULL);
+	scm_c_export("is-accent", NULL);
+	scm_c_export("is-punc", NULL);
+	scm_c_export("is-digit", NULL);
+	scm_c_export("is-symbol", NULL);
 }
 
 extern "C"
